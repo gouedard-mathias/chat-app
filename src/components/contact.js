927490e6c7df2c932988419bae4aac7f3e482aa2
gouedard-mathias <m-gouedard40500@hotmail.fr>
2021-01-25 11:16:15 +0100

    React 05 - Le state

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,20 +2,34 @@ import React from 'react';
 import './contact.css';
 import PropTypes from 'prop-types';
 
+class Contact extends React.Component
+{
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: props.online
+        }
+    }
 
-function Contact(props) {
-    return (
-        <div className="Contact">
-            <img className="avatar" src={props.avatar} alt="avatar icon" />
-            <div className="name-online">
-                <h4 className="name">{props.name}</h4>
-                <div className="status">
-                    <div className={props.online ? 'status-online' : 'status-offline'}> </div>
-                    <p className="status-text">{props.online ? "online" : "offline"}</p>
+    render() {
+        return (
+            <div className="Contact">
+                <img className="avatar" src={this.props.avatar} alt="avatar icon" />
+                <div className="name-online">
+                    <h4 className="name">{this.props.name}</h4>
+                    <div className="status"
+                         onClick={event => {
+                             const newOnline = !this.state.online;
+                             this.setState({ online: newOnline });
+                         }}
+                    >
+                        <div className={this.state.online ? 'status-online' : 'status-offline'}> </div>
+                        <p className="status-text">{this.state.online ? "online" : "offline"}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 Contact.propTypes = {
